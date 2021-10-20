c8602e83e5d0fba3cc7b4f3ff5935a07c5bb03ae
Lucas Souza <lucasp.sdev@gmail.com>
2021-10-20 09:07:34 -0300

    fix: deploy port error

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,3 @@
 import { serverHttp } from "./app";
 
-serverHttp.listen(4000, () => console.log('🦥 Server is running on PORT 4000'))
+serverHttp.listen(process.env.PORT || 4000, () => console.log('🦥 Server is running on PORT 4000'))
